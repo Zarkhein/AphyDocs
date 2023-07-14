7e00c42d5359e04e4f4081ddf95715b5a759b690
Zarkhein <63240197+Zarkhein@users.noreply.github.com>
2023-07-14 13:23:18 +0200

    init
    Projet can be Download

---
+++ AphyFormDossier/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace AphyFormDossier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_open = new System.Windows.Forms.Button();
+            this.FDB = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtDays = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(12, 12);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(165, 43);
+            this.btn_open.TabIndex = 0;
+            this.btn_open.Text = "Ouvrir";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 61);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(165, 43);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(183, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(307, 426);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(496, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(292, 426);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 110);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(165, 43);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtDays
+            // 
+            this.txtDays.Location = new System.Drawing.Point(12, 190);
+            this.txtDays.Name = "txtDays";
+            this.txtDays.Size = new System.Drawing.Size(165, 43);
+            this.txtDays.TabIndex = 3;
+            this.txtDays.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Days";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDays);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btn_open);
+            this.Name = "Form1";
+            this.Text = "AphyPics";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.FolderBrowserDialog FDB;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.RichTextBox txtDays;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AphyFormDossier/Form1.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AphyFormDossier
+{
+    public partial class Form1 : Form
+    {
+
+        public PictureBox[] pictureBoxes;
+        public Label[] labelTextPicture;
+        FileInfo[] myfiles;
+        public int startPicture = 0;
+        public int dayLimits = 5;
+        public int j = 0;
+        public FlowLayoutPanel flowLayoutPanel1 = new FlowLayoutPanel();
+        public FlowLayoutPanel flowLayoutPanel2 = new FlowLayoutPanel();
+
+
+        public Form1()
+        {   
+            flowLayoutPanel1.Location = new Point(183, 12);
+            flowLayoutPanel1.Size = new Size(307, 426);
+            flowLayoutPanel1.AutoScroll = true;
+            this.Controls.Add(flowLayoutPanel1);            
+
+            flowLayoutPanel2.Location = new Point(496, 12);
+            flowLayoutPanel2.Size = new Size(292, 426);
+            flowLayoutPanel2.AutoScroll = true;
+            this.Controls.Add(flowLayoutPanel2);
+            InitializeComponent();
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {            
+            flowLayoutPanel1.Controls.Clear();
+            flowLayoutPanel2.Controls.Clear();
+            
+            FDB = new FolderBrowserDialog();
+            
+            if (FDB.ShowDialog() == DialogResult.OK && (!(string.IsNullOrEmpty(txtDays.Text))))
+            {
+                
+                DirectoryInfo path = new DirectoryInfo(FDB.SelectedPath);
+                string[] extensionFiles = { ".jpg", ".png", ".webp"};
+                myfiles = path.GetFiles("*", SearchOption.AllDirectories)
+                    .Where(file => extensionFiles.Contains(file.Extension, StringComparer.OrdinalIgnoreCase))
+                    .ToArray();
+
+                pictureBoxes = new PictureBox[myfiles.Length];
+                labelTextPicture = new Label[myfiles.Length];
+
+                for (int i = 0 ; i < pictureBoxes.Length; i++)
+                {
+                    try
+                    {
+                        DateTime test = myfiles[i].CreationTime;
+                        long elapsedTime = (DateTime.Now).Ticks - test.Ticks;
+                        TimeSpan testSpan = new TimeSpan(elapsedTime);
+
+                        if(testSpan.TotalDays >= Convert.ToInt16(txtDays.Text)) {
+                            Console.WriteLine(j);
+                            
+                            pictureBoxes[j] = new PictureBox();
+                            labelTextPicture[j] = new Label();
+
+                            //picture                        
+                            pictureBoxes[j].Location = new Point(225, 50);
+                            pictureBoxes[j].Size = new Size(50, 50);
+                            pictureBoxes[j].SizeMode = PictureBoxSizeMode.StretchImage;
+                            pictureBoxes[j].Image = Image.FromFile(myfiles[j].FullName);
+
+                            //label
+                            labelTextPicture[j].Text = myfiles[j].Name;
+
+                            flowLayoutPanel2.Controls.Add(labelTextPicture[j]);
+                            flowLayoutPanel1.Controls.Add(pictureBoxes[j]);
+                            j++;
+
+                        }
+                    }
+                    catch (OutOfMemoryException ex)
+                    {
+                        MessageBox.Show($"Erreur lors du chargement de l'image {myfiles[i].Name}: {ex.Message}");
+                    }                    
+                }                
+            }
+            else
+            {
+                MessageBox.Show("Erreur vous avez oublie de remplir les champs !");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                try
+                {
+                    pictureBoxes[i].Image.Dispose();
+                    pictureBoxes[i].Image = null;
+                    labelTextPicture[i].Text = null;
+                }
+                catch (NullReferenceException err)
+                {
+                    MessageBox.Show($"Erreur de suppression : Auncune image a été chargé");
+                    Console.WriteLine(err.Message);
+                    break;
+                }
+            }         
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Vous etes sur de supprimer les images ?", "Suppresion Image", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    try
+                    {
+                        pictureBoxes[i].Image.Dispose();
+                        File.Delete(myfiles[i].FullName);
+                        pictureBoxes[i].Image = null;
+                        labelTextPicture[i].Text = null;
+                    }
+                    catch(NullReferenceException err)
+                    {
+                        MessageBox.Show($"Erreur de suppression : Auncune image a été chargé");
+                        Console.WriteLine(err.Message);
+                        break;
+                    }
+                }          
+            }            
+        }
+
+        private void txtDays_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }    
+}
